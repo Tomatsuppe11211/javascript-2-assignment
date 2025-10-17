7547bf5cf07b5a69d871733ba7e9dbb5a531304a
Tomatsuppe11211 <142995184+Tomatsuppe11211@users.noreply.github.com>
2025-10-17 20:02:20 +0200

    Retrieving social posts
    I have now successfully managed to access the posts that is currently out. Per now it is 100 posts i can choose to work with, so i will work on that next week.

--- timeturner/post/feed.ts
+++ timeturner/post/feed.ts
@@ -1,8 +1,15 @@
 const URL = 'https://v2.api.noroff.dev/social/posts'
 
+
+
+
+
 interface profileToken {accessToken: string}
 
 const getProfile = localStorage.getItem('profileData')
+
+//returning to landing page if no user is logged in
+if(!getProfile){window.location.href = '../index.html'}
 
 let profile: profileToken | null = null 
 
@@ -15,25 +22,56 @@ console.log(profile)
 
 
 
-async function getPosts(){
-    console.log('Getting all posts...')
+//Creating an api-key
+async function createApiKey(){
+    const response = await fetch('https://v2.api.noroff.dev/auth/create-api-key', {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'Application/json'
+        },
+        body: JSON.stringify({name: `current apiKey`})
+    })
 
-    
+    const data = await response.json()
+    const currentKey = data.data.key
+    localStorage.setItem('CurrentKey', currentKey)
+    return data.data.key
+}
+
+createApiKey()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+async function getPosts(){
+    const apiKey = await createApiKey()
+    localStorage.getItem('currentKey')
 
     if(!token){
         console.log('There was an error fetching posts')
         return
     }
-    
-
-    console.log('token: ', token)
 
     try{
         const response = await fetch(URL, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
+                'Authorization': `Bearer ${token}`,
+                'X-Noroff-API-Key': apiKey
             }
         })
 
@@ -49,4 +87,5 @@ async function getPosts(){
         console.log('error: ', error)
     }
 }
+
 getPosts()
