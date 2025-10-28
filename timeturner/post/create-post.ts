d0d28cfb5bd0d34c40d88927df8e5268416f3de2
Tomatsuppe11211 <142995184+Tomatsuppe11211@users.noreply.github.com>
2025-10-28 12:56:35 +0100

    almost finished creating post function
    The create post is finished and you can post but there are still some error messages that need handling in typescript file.

--- timeturner/post/create-post.ts
+++ timeturner/post/create-post.ts
@@ -1,1 +1,80 @@
-console.log('Hello!')
+const postTitle = document.getElementById('createPostTitle') as HTMLInputElement
+const postBody = document.getElementById('createPostBody') as HTMLTextAreaElement
+const postImageUrl = document.getElementById('postImageUrl') as HTMLInputElement
+const postImageAlt = document.getElementById('postImageDescription') as HTMLInputElement
+const createPostButton = document.getElementById('createPostButton') as HTMLButtonElement
+
+
+const profileData = localStorage.getItem('profileData')
+
+interface profileToken{accessToken: string, name: string}
+
+let profile: profileToken | null = null
+
+if (profileData){profile = JSON.parse(profileData)}
+
+const token = profile?.accessToken
+
+console.log(profile)
+
+const API_KEY = localStorage.getItem('CurrentKey') || ''
+console.log(API_KEY)
+
+
+createPostButton.addEventListener('click', async function(e){
+    e.preventDefault()
+
+    //fetching values
+    const title = postTitle.value
+    let body = postBody.value
+    let imageURL = postImageUrl.value
+    let imageAlt = postImageAlt.value
+
+
+    console.log(`Title: ${title}`)
+    console.log(`body: ${body}`)
+    console.log(`image url: ${imageURL}`)
+    console.log(`Image alt: ${imageAlt}`)
+
+    if(!title){alert('Title is required')}
+
+    const postData: any = {
+        title,
+        body: body || undefined,
+        tags: []
+    }
+
+    if(imageURL){
+        postData.media = {
+            url: imageURL,
+            alt: imageAlt || ''
+        }
+    }
+
+    try{
+        const response = await fetch('https://v2.api.noroff.dev/social/posts', {
+            method: 'POST',
+            
+            headers: {
+                'Authorization': `Bearer ${token}`, 
+                'Content-Type': 'application/json',
+                'X-Noroff-API-Key': API_KEY
+            }, 
+            
+            body: JSON.stringify(postData)
+        })
+
+        if(!response.ok){
+            console.log('Something went wrong! with creating this post:')
+            console.log(response.text)
+            return
+        }
+
+        alert('Post created!')
+
+        window.location.href = '/index.html'
+    } catch(error){
+        console.error(error)
+    }
+    
+})
