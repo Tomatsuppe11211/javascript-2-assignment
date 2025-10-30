ccf7e35a4ac7ecb3b9880076f3196526e5c85365
Tomatsuppe11211 <142995184+Tomatsuppe11211@users.noreply.github.com>
2025-10-30 20:07:16 +0100

    ALmost done with updating posts
    I have worked on the edit post page in typescript but due to internet porblems now i can't tell if it works yet. I will check it later.

--- timeturner/post/edit-post.ts
+++ timeturner/post/edit-post.ts
@@ -1,8 +1,8 @@
-const postTitle = document.getElementById('editPostTitle')
-const postBody = document.getElementById('editPostBody')
-const postImageUrl = document.getElementById('postImageurl')
-const postImageAlt = document.getElementById('postImageDescription')
-const saveChangesButton = document.getElementById('editPostbutton')
+const postTitle = document.getElementById('editPostTitle') as HTMLInputElement
+const postBody = document.getElementById('editPostBody') as HTMLInputElement
+const postImageUrl = document.getElementById('postImageUrl') as HTMLInputElement
+const postImageAlt = document.getElementById('postImageDescription') as HTMLInputElement
+const saveChangesButton = document.getElementById('editPostButton') as HTMLInputElement
 
 
 let profileData = sessionStorage.getItem('profileData')
@@ -40,10 +40,71 @@ async function getPost(){
         }
 
         const data = await response.json()
+        const oldContent = data.data
         console.log(data)
+
+        postTitle.value = oldContent.title
+
+        if(oldContent.body && oldContent.body !== null){
+            postBody.value = oldContent.body
+        }
+
+        if(oldContent.media.url && oldContent.media.url !== null){
+            postImageUrl.value = oldContent.media.url
+        }
+
+        if(oldContent.media.alt && oldContent.media.alt !== null){
+            postImageAlt.value = oldContent.media.alt
+        }
+        
+
     } catch(error){
         console.error(error)
     }
 }
-
 getPost()
+
+
+
+
+
+saveChangesButton.addEventListener('click', async function(e){
+    e.preventDefault()
+    
+    if(!postTitle){alert('Title is required')}
+
+
+    const newPostData: any = {
+        title: postTitle.value,
+        body: postBody.value || undefined,
+        tags: []
+    }
+
+    if(postImageUrl){
+        newPostData.media = {
+            url: postImageUrl.value,
+            alt: postImageAlt.value || ''
+        }
+    }
+
+
+    const response = await fetch(`https://v2.api.noroff.dev/social/posts/${editId}`, {
+        method: 'PUT',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json', 
+            'X-Noroff-API-Key': apiKey
+        },
+        body: JSON.stringify({newPostData})
+    })
+    
+    if(!response.ok){
+        const errorMessage = response.text()
+        console.log(errorMessage)
+        return
+    }
+
+    alert('Saved changes!')
+
+    window.location.href = '/index.html'
+})
